e038462ee2d096f8ef65326b6c7ee0477dff21a9
lpatrizio <luca12@hotmail.ca>
2023-12-13 16:49:06 -0500

    Day 13

---
+++ Day13/Day13.py
@@ -0,0 +1,126 @@
+import numpy  
+import collections
+file = open("input", "r")
+
+#part 1
+'''sample = file.readline()
+load = []
+total = 0
+while sample:
+    if sample!='\n':
+        load.append(list(sample.strip()))
+    else:
+        saved = total
+        for i in range(0, len(load)+1):   #rows check
+            try:
+                if load[i]==load[i+1] and "#" in load[i]:  #this is our start point so we go from here
+                    mirrored = True
+                    try:    #we'll eventually hit either the top or bottom limit and index error so we'll use that to our advantage
+                        for j in range(1, i+1):
+                            if (load[i+1+j]==load[i-j]):
+                                mirrored=True
+                            else:
+                                mirrored = False
+                                break
+                    except:
+                        pass
+                    if mirrored==True:
+                            total+=(i+1)*100
+                            break
+            except:
+                pass
+        if not (total!=saved):   #assuming every map has EITHER a vertical or horizontal, not both
+            trns = numpy.transpose(load).tolist()
+            for i in range(0, len(trns)+1):   #column check
+                try:
+                    if trns[i]==trns[i+1] and "#" in trns[i]:
+                        mirrored = True
+                        try:
+                            for j in range(1, i+1):
+                                    if (trns[i+1+j]==trns[i-j]):
+                                        mirrored=True
+                                    else:
+                                        mirrored = False
+                                        break
+                        except:
+                            pass
+                        if mirrored==True:
+                            total+=(i+1)
+                            break
+                except:
+                    pass
+        load = []
+    sample = file.readline()
+print(total)'''
+
+
+
+#part 2
+sample = file.readline()
+load = []
+total = 0
+while sample:
+    if sample!='\n':
+        load.append(list(sample.strip()))
+    else:
+        smudgerepair = False
+        saved = total
+        for i in range(0, len(load)+1):   #rows check
+            try:
+                count = collections.Counter(numpy.array(load[i]) == numpy.array(load[i+1]))[False]
+                if count <= 1:  #this is our start point so we go from here
+                    if count==1: 
+                        smudgerepair=True
+                    mirrored = True
+                    try:    #we'll eventually hit either the top or bottom limit and index error so we'll use that to our advantage
+                        for j in range(1, i+1):
+                            if (load[i+1+j]==load[i-j]):
+                                mirrored=True
+                            else:
+                                counter = collections.Counter(numpy.array(load[i+1+j]) == numpy.array(load[i-j]))[False]
+                                if counter > 1:
+                                    mirrored = False
+                                    smudgerepair = False
+                                    break
+                                else:
+                                    smudgerepair=True
+                                    pass
+                    except:
+                        pass
+                    if mirrored==True and smudgerepair==True:
+                            total+=(i+1)*100
+                            break
+            except:
+                pass
+        if not (total!=saved):   #assuming every map has EITHER a vertical or horizontal, not both
+            trns = numpy.transpose(load).tolist()
+            for i in range(0, len(trns)+1):   #column check
+                try:
+                    count = collections.Counter(numpy.array(trns[i]) == numpy.array(trns[i+1]))[False]
+                    if count==1: 
+                        smudgerepair=True
+                    if count<=1:
+                        mirrored = True
+                        try:
+                            for j in range(1, i+1):
+                                if (trns[i+1+j]==trns[i-j]):
+                                    mirrored=True
+                                else:
+                                    counter = collections.Counter(numpy.array(trns[i+1+j]) == numpy.array(trns[i-j]))[False]
+                                    if counter > 1:
+                                        mirrored = False
+                                        smudgerepair = False
+                                        break
+                                    else:
+                                        smudgerepair=True
+                                        pass
+                        except:
+                            pass
+                        if mirrored==True and smudgerepair==True:
+                            total+=(i+1)
+                            break
+                except:
+                    pass
+        load = []
+    sample = file.readline()
+print(total)
